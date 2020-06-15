224cea1dee8f4554ee9eb127f81196d5fb80d82e
Soneya Binta Hossain <54991949+soneyahossain@users.noreply.github.com>
2020-06-14 22:11:12 -0400

    Update commandLine.sh

--- test/commandLine.sh
+++ test/commandLine.sh
@@ -5,7 +5,7 @@ clang -emit-llvm -fno-discard-value-names classify.cpp -c -o PUT.bc
 /Users/soneyabintahossain/llvm_project/llvm-project/build/bin/opt -load=/Users/soneyabintahossain/llvm_project/llvm-project/build/lib/LLVMSTGInstrument.dylib -symbTraceInstrumentation PUT.bc -o IPUT.bc
 /Users/soneyabintahossain/llvm_project/llvm-project/build/bin/llvm-dis IPUT.bc -o IPUT.ll
 
-clang++ -std=c++11 -emit-llvm ../stg_lib/stg.cpp -c -o stg.bc
+clang++ -std=c++11 -emit-llvm ../lib/stg.cpp -c -o stg.bc
 /Users/soneyabintahossain/llvm_project/llvm-project/build/bin/llvm-link IPUT.bc stg.bc -o linked.bc
 /Users/soneyabintahossain/llvm_project/llvm-project/build/bin/llc -filetype=obj linked.bc
 clang++ -o a.out  linked.o
