fae9b356f0aef6b34f5fd91d280ae785a375dc1b
Anh <an7s@virginia.edu>
2020-10-09 17:06:52 -0700

    Robustify installation

--- util/install_clang.sh
+++ util/install_clang.sh
@@ -2,22 +2,25 @@
 # and other needed tools
 
 #
-# Install latest LLVM (currently at 10)
+# Install latest LLVM (currently at 11)
 #
-bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
-# Alternatively, to install a specific LLVM version
-#    wget https://apt.llvm.org/llvm.sh
-#    chmod +x llvm.sh
-#    sudo ./llvm.sh <version number>
+VERSION=11
+
+# bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh $VERSION
+rm ./llvm.sh
 
 # Update links
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 100
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-10 100
-sudo update-alternatives --install /usr/bin/opt opt /usr/bin/opt-10 100
-sudo update-alternatives --install /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-10 100
-sudo update-alternatives --install /usr/bin/llvm-dis llvm-dis /usr/bin/llvm-dis-10 100
-sudo update-alternatives --install /usr/bin/llc llc /usr/bin/llc-10 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$VERSION 100
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$VERSION 100
+sudo update-alternatives --install /usr/bin/opt opt /usr/bin/opt-$VERSION 100
+sudo update-alternatives --install /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-$VERSION 100
+sudo update-alternatives --install /usr/bin/llvm-dis llvm-dis /usr/bin/llvm-dis-$VERSION 100
+sudo update-alternatives --install /usr/bin/llc llc /usr/bin/llc-$VERSION 100
 
 #
 # Install cmake
--- util/install_stgi.sh
+++ util/install_stgi.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -x
+
 usage()
 {
 	echo "Make sure to define the following environment variables:"
