3b71b36f8c653b9910b9ca471c8aedce8e8d590c
Soneya Binta Hossain <54991949+soneyahossain@users.noreply.github.com>
2021-01-26 11:43:17 -0500

    latest stgi compatible stg constraints

--- test_programs/llvm_intrinsic_test/llvm.log_test/llvm.log_test.cpp
+++ test_programs/llvm_intrinsic_test/llvm.log_test/llvm.log_test.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <float.h>
-#include "stg_lib/stg.h"
+#include "stgi/stg.h"
 
 
 bool isGreaterThan(double x, float y)
@@ -16,8 +16,8 @@ bool isGreaterThan(double x, float y)
 int main()
 {
     double x=3.5; float y=5.6;
- scanf("%lf",&x);
-   scanf("%f",&y);
+    scanf("%lf",&x);
+    scanf("%f",&y);
 
     stg_begin_test();
 
