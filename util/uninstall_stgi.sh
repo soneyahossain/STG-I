22b1fd99fc68118bcb43346fc8102796eafb9707
Anh <an7s@virginia.edu>
2021-02-18 18:32:09 -0800

    WIP: test runners

--- util/uninstall_stgi.sh
+++ util/uninstall_stgi.sh
@@ -5,7 +5,8 @@ remove_if_exists()
 	to_remove=$1
 	if [ -f $to_remove ]; then
 		echo "Removing $to_remove"
-		sudo rm $to_remove
+#		sudo rm $to_remove
+		rm $to_remove
 	fi
 }
 
@@ -13,7 +14,7 @@ if [ ! -z $STGI_HOME ]; then
 	pushd $STGI_HOME/lib >/dev/null
 	if [ -f Makefile ]; then
 		make clean >/dev/null 2>&1
-		make distclean >/dev/null 2>&1
+		sudo -E make distclean >/dev/null 2>&1
 	fi
 	popd >/dev/null
 
@@ -28,7 +29,7 @@ verify_uninstall()
 {
 	success="yes"
 	echo "Verifying STG-I uninstallation..."
-	for f in $Stgi_files
+	for f in $stgi_files
 	do
 		if [ -f $f ]; then
 			echo "Warning: $f was not removed successfully"
@@ -46,15 +47,15 @@ verify_uninstall()
 #
 # inventory of potential STG-I files
 #
-stgi_files="/usr/local/lib/stg.bc /usr/local/include/stgi/stg.h /usr/local/lib/libSTGInstrumenter.so"
+stgi_files="/usr/local/lib/stg.bc /usr/local/include/stgi/stg.h /usr/local/lib/libLLVMSTGInstrumenter.so"
 if [ ! -z $STGI_LIB ]; then
-	stgi_files="$stgi_files $STGI_LIB/stg.bc $STGI_LIB/libSTGInstrumenter.so"
+	stgi_files="$stgi_files $STGI_LIB/stg.bc $STGI_LIB/libLLVMSTGInstrumenter.so"
 fi
 if [ ! -z $STGI_INC ]; then
 	stgi_files="$stgi_files $STGI_INC/stg.h "
 fi
 
-for f in $Stgi_files
+for f in $stgi_files
 do
 	remove_if_exists $f
 done
