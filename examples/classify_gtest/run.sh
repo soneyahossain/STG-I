fdc09d2204725567dc9138aeffadb352047ac22c
soneya <sh7hv@virginia.edu>
2021-03-23 17:15:25 -0400

    added STGI_INC in run.sh

--- examples/classify_gtest/run.sh
+++ examples/classify_gtest/run.sh
@@ -8,7 +8,7 @@ filepath="$STGI_EXAMPLE_DIR"/classify_gtest
 clang++ -std=c++14 -I$STGI_INC/.. -c -Wall -I /usr/src/gtest/include -emit-llvm -fno-discard-value-names "$filepath"/classify.cpp -o PUT.bc
 opt -load="$STGI_LIB"/LLVMSTGInstrumenter.so -STGInstrumenter PUT.bc -o IPUT.bc
 
-clang++ -std=c++14 -c -Wall -I /usr/src/gtest/include -emit-llvm -fno-discard-value-names "$filepath"/classify_test.cpp -o classify_test.bc
+clang++ -std=c++14 -I$STGI_INC/.. -c -Wall -I /usr/src/gtest/include -emit-llvm -fno-discard-value-names "$filepath"/classify_test.cpp -o classify_test.bc
 opt -load="$STGI_LIB"/LLVMSTGInstrumenter.so -STGInstrumenter -partial-instrument=true classify_test.bc -o Iclassify_test.bc
 llvm-link  IPUT.bc Iclassify_test.bc "$STGI_LIB"/stg.bc -o linked.bc
 llc -filetype=obj linked.bc
